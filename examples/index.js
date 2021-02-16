fad7819b758c8800834810575591efb9c2d6a4d3
talhag3 <khokhar95@yahoo.com>
2021-02-16 19:06:02 +0500

    issue with async

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,6 @@
 var ytvid = require('../src/index')
+console.log('aBC')
 
 console.log(ytvid('Wy9q22isx3U'))
+
+console.log('XYZ')
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 
-//'use strict';
-
+'use strict';
 var debug = require('debug')('yt-gets');
-const { default: axios } = require('axios');
-const qs = require('query-string')
-var fs = require('fs')
+var fs = require('fs');
 var helper = require('./helpers/index');
+
 /**
  * Name of this module
  */
@@ -18,17 +16,28 @@ var ID= 'Wy9q22isx3U'
  */
 var SERVICE_ENDPOINT = 'https://youtube.com/get_video_info?video_id=';
 
-var id = null
+var ID = null
 
 async function fetch(id) {
-    id = id
+    ID = id
     var res = await helper.request({type:'get',url:SERVICE_ENDPOINT+id})
     var parsed = helper.qstoObj(res)
+    parsed.fflags = helper.qstoObj(parsed.fflags)
     parsed.player_response = JSON.parse(parsed.player_response)
-    fs.writeFileSync(Date.now()+'_ytres.json',JSON.stringify(parsed))
-    console.log('done')
     
-    
-    return {parsed:'parsed'}
+    var nomalized = {
+        videoId : parsed.player_response.videoDetails.videoId,
+        description : parsed.player_response.videoDetails.shortDescription,
+        channelId : parsed.player_response.videoDetails.channelId,
+        averageRating : parsed.player_response.videoDetails.averageRating,
+        viewCount : parsed.player_response.videoDetails.viewCount,
+        author : parsed.player_response.videoDetails.author,
+        title : parsed.player_response.videoDetails.title,
+        keywords : parsed.player_response.videoDetails.keywords,
+        images : parsed.player_response.videoDetails.thumbnail.thumbnails,
+    }
+    console.log(nomalized)
+    //fs.writeFileSync(Date.now()+'_ytres.json',JSON.stringify(parsed))
+    return nomalized
 }
 module.exports = fetch;
