69b76ae7c0d04fdf8cd898ff1975e6d31819dc87
talhag3 <khokhar95@yahoo.com>
2021-02-17 11:42:41 +0500

    example update

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,6 @@
 var ytvid = require('../src/index')
-console.log('aBC')
-
+console.log('ABC')
+// I want this function to return data not promise pending 
 console.log(ytvid('Wy9q22isx3U'))
 
 console.log('XYZ')
